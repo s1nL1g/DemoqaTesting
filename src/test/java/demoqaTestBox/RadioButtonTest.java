931498a35921c4420fe8e2438975d9defbf61a52
Bogdan <bodaginaylo33@gmail.com>
2023-04-14 01:27:57 +0300

    updated file "RadioButtonTest.java"

--- src/test/java/demoqaTestBox/RadioButtonTest.java
+++ src/test/java/demoqaTestBox/RadioButtonTest.java
@@ -2,24 +2,47 @@ package demoqaTestBox;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static java.lang.Thread.sleep;
 
 public class RadioButtonTest {
-    @Test
-    public void chekLoginTestBox() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+    WebDriver driver = new ChromeDriver();
+
+    @BeforeClass
+    public void setUp() {
         driver.manage().window().maximize();
         driver.get("https://demoqa.com/radio-button");
+    }
+
+    @Test
+    public void chekLoginTestBox() throws InterruptedException {
+
+
+        WebElement radioButtonNo = driver.findElement(By.xpath("//input[@id='noRadio']"));
+
+        Assert.assertFalse(radioButtonNo.isEnabled(), "RadioButton 'No' should be disabled.");
 
         driver.findElement(By.xpath("//label[@for='yesRadio']")).click();
 
         Assert.assertTrue(driver.findElement(By.xpath("//span[@class='text-success']")).isDisplayed());
 
-        sleep(2000);
+        driver.findElement(By.xpath("//label[@for='impressiveRadio']")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//span[@class='text-success']")).isDisplayed());
+
+        sleep(1000);
+    }
+
+    @AfterClass
+    public void enging() {
         driver.quit();
     }
+
+
 }

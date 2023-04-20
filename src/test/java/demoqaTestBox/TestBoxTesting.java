648089d9aef96eeaeebb091a9c039ceb4439e724
Bogdan <bodaginaylo33@gmail.com>
2023-04-20 15:23:32 +0300

    updated file "TestBoxTesting.java"

--- src/test/java/demoqaTestBox/TestBoxTesting.java
+++ src/test/java/demoqaTestBox/TestBoxTesting.java
@@ -3,20 +3,28 @@ package demoqaTestBox;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static java.lang.Thread.sleep;
+import static org.testng.Assert.*;
 
 public class TestBoxTesting {
 
+    WebDriver driver = new ChromeDriver();
+
+    @BeforeClass
+    public void setUp() {
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/broken");
+    }
+
     @Test
     public void chekLoginTestBox() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://demoqa.com/text-box");
-
         driver.findElement(By.xpath("//input[@id='userName']")).sendKeys("Roman Roman");
         driver.findElement(By.xpath("//input[@id='userEmail']")).sendKeys("email1@gmail.com");
         driver.findElement(By.xpath("//textarea[@id='currentAddress']")).sendKeys("hshbqsbbxbshq");
@@ -28,9 +36,14 @@ public class TestBoxTesting {
         sleep(1000);
         driver.findElement(By.xpath("//button[@id='submit']")).click();
 
-        Assert.assertEquals(driver.findElement(By.xpath("//p[@id='name']")).getText(), "Name:Roman Roman");
+        assertEquals(driver.findElement(By.xpath("//p[@id='name']")).getText(), "Name:Roman Roman");
 
         sleep(1500);
+
+    }
+
+    @AfterClass
+    public void tearDown() {
         driver.quit();
     }
 }

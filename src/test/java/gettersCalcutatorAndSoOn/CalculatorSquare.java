e6bed15e1bbe87f0e1469263a1fd098c633250c7
Bogdan <bodaginaylo33@gmail.com>
2023-04-25 12:05:08 +0300

    added 2 files with homework

---
+++ src/test/java/gettersCalcutatorAndSoOn/CalculatorSquare.java
@@ -0,0 +1,28 @@
+package gettersCalcutatorAndSoOn;
+
+public class CalculatorSquare {
+    public class squareOfNumbers {
+
+        public static double calculateSquareRoot(byte num) {
+            return Math.sqrt(num);
+        }
+
+        public static double calculateSquareRoot(short num) {
+            return Math.sqrt(num);
+        }
+
+        public static double calculateSquareRoot(int num) {
+            return Math.sqrt(num);
+        }
+
+        public static double calculateSquareRoot(float num) {
+            return Math.sqrt(num);
+        }
+
+        public static double calculateSquareRoot(double num) {
+            return Math.sqrt(num);
+        }
+    }
+
+}
+
---
+++ src/test/java/gettersCalcutatorAndSoOn/GettersSetters.java
@@ -0,0 +1,97 @@
+package gettersCalcutatorAndSoOn;
+
+public class GettersSetters {
+    public class Person {
+        private String name;
+        private int age;
+        private String address;
+        private String phoneNumber;
+        private String email;
+        private String occupation;
+        private boolean isMarried;
+        private int salary;
+        private boolean hasChildren;
+        private String favoriteColor;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getOccupation() {
+            return occupation;
+        }
+
+        public void setOccupation(String occupation) {
+            this.occupation = occupation;
+        }
+
+        public boolean isMarried() {
+            return isMarried;
+        }
+
+        public void setMarried(boolean isMarried) {
+            this.isMarried = isMarried;
+        }
+
+        public int getSalary() {
+            return salary;
+        }
+
+        public void setSalary(int salary) {
+            this.salary = salary;
+        }
+
+        public boolean hasChildren() {
+            return hasChildren;
+        }
+
+        public void setHasChildren(boolean hasChildren) {
+            this.hasChildren = hasChildren;
+        }
+
+        public String getFavoriteColor() {
+            return favoriteColor;
+        }
+
+        public void setFavoriteColor(String favoriteColor) {
+            this.favoriteColor = favoriteColor;
+        }
+    }
+
+}
